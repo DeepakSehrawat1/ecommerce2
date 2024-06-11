5399e1a5cbfe9977ea2fd0a15636631b5f1bed65
DeepakSehrawat1 <deepaksehrawat150@gmail.com>
2024-06-11 16:23:26 +0530

    find

--- models/product.js
+++ models/product.js
@@ -21,7 +21,35 @@ class Product {
       });
   }
 
-  fetchAll() {}
+  static fetchAll() {
+    const db = getdb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getdb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
 
 /*const Sequelize = require('sequelize');
